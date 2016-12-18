5244d788f27b50b79052e91797738ae92c912471
Tony Shen <fengzhizi715@126.com>
2016-12-18 13:59:32 +0800

    update BaseActivity

--- library/src/main/java/cn/salesuite/mlogcat/app/BaseActivity.java
+++ library/src/main/java/cn/salesuite/mlogcat/app/BaseActivity.java
@@ -3,12 +3,11 @@
  */
 package cn.salesuite.mlogcat.app;
 
-import java.lang.ref.WeakReference;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
+
+import cn.salesuite.saf.app.SafeHandler;
 import cn.salesuite.saf.inject.Injector;
 import cn.salesuite.saf.log.L;
 import cn.salesuite.saf.utils.ToastUtils;
@@ -20,12 +19,12 @@ import cn.salesuite.saf.utils.ToastUtils;
  */
 public class BaseActivity extends Activity {
 	
-	protected Handler mHandler = new MyHandler(this);
+	protected Handler mHandler = new SafeHandler(this);
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		L.init(BaseActivity.class);
+		L.init(this.getClass());
 	}
 	
 	public void setContentView(int layoutResID) {
@@ -45,23 +44,5 @@ public class BaseActivity extends Activity {
 	 */
 	protected void toast(int resId) {
 		ToastUtils.showShort(this, resId);
-	}
-	
-	/**
-	 * 防止内部Handler类引起内存泄露
-	 * @author Tony Shen
-	 *
-	 */
-    public static class MyHandler extends Handler{
-	    private final WeakReference<Activity> mActivity;
-	    public MyHandler(Activity activity) {
-	        mActivity = new WeakReference<Activity>(activity);
-	    }
-	    @Override
-	    public void handleMessage(Message msg) {
-	        if(mActivity.get() == null) {
-	            return;
-	        }
-	    }
 	}
 }
